36c19b73cf27808af80ad3f8da490c84d31fe995
David Ackerman <david.w.ackerman@gmail.com>
2016-03-22 14:25:03 -0700

    Properly joins a channel, and makes random numbers
    Code is hacky, and in need of some refactoring

--- src/main/java/IrcMain.java
+++ src/main/java/IrcMain.java
@@ -1,7 +1,12 @@
+import sun.misc.Regexp;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Random;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class IrcMain {
 
@@ -30,14 +35,29 @@ public class IrcMain {
 
         write("NICK", nick);
         write("USER", username + " 0 * :" + realName);
-        write("JOIN", "#reddit-dailyprogrammer");
 
         while (in.hasNext()) {
             String serverMessage = in.nextLine();
             System.out.println("<<< " + serverMessage);
+            Pattern randomNumberRegex = Pattern.compile(".* PRIVMSG ([^\\s]*) .*random (\\d+)$");
+            Matcher m = randomNumberRegex.matcher(serverMessage);
+
             if (serverMessage.startsWith("PING")) {
                 String pingContents = serverMessage.split(" ", 2)[1];
                 write("PONG", pingContents);
+            } else if (serverMessage.contains(" 376 ")) {
+                write("JOIN", "#reddit-dailyprogrammer,#botters-test");
+            } else if (serverMessage.contains(" 366 ") && serverMessage.contains("#botters-test")) {
+                write("PRIVMSG", "#botters-test :Test message");
+            } else if (m.matches()) {
+                String channel = m.group(1);
+                try {
+                    int bound = Integer.parseInt(m.group(2));
+                    int randomNumber = new Random().nextInt(bound);
+                    write("PRIVMSG", channel + " :Your random number is " + randomNumber);
+                } catch (NumberFormatException e) {
+                    write("PRIVMSG", channel + " :Hmm, that didn't look like a number to me.");
+                }
             }
         }
 
